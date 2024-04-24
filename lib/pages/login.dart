dc0e3e5ce2bb3e010a87b19c59daee2330b5440a
Robin <racastro218@gmail.com>
2024-04-24 14:01:37 -0500

    login_click

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,6 +7,7 @@ import 'package:autogestion_tecnico/global/globals.dart';
 import 'package:autogestion_tecnico/global/custom_show_dialog.dart';
 import 'package:autogestion_tecnico/services/services.dart';
 import 'package:autogestion_tecnico/widgets/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -25,6 +26,115 @@ class _LoginPageState extends State<LoginPage> {
 
     final uiProvider = Provider.of<UiProvider>(context);
 
+    // return Scaffold(
+    //   body: GestureDetector(
+    //     onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+    //     child: SingleChildScrollView(
+    //       child: SafeArea(
+    //         child: Stack(
+    //           children: [
+    //             Row(
+    //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //               children: [
+    //                 Container(
+    //                     margin: EdgeInsets.only(left: mq.width * 0.02),
+    //                     width: mq.width * 0.20,
+    //                     height: mq.width * 0.20,
+    //                     decoration: const BoxDecoration(
+    //                         image: DecorationImage(
+    //                             image: AssetImage("assets/img/logo.png")))),
+    //                 IconButton(
+    //                     padding: const EdgeInsets.all(2),
+    //                     onPressed: () {
+    //                       ScaffoldMessenger.of(context).showSnackBar(
+    //                           const SnackBar(
+    //                               content:
+    //                                   Text('Próximamente en funcionamiento.')));
+    //                     },
+    //                     icon: const Icon(Icons.help_rounded))
+    //               ],
+    //             ),
+    //             SizedBox(
+    //               width: mq.width * 1,
+    //               height: mq.height * 0.9,
+    //               child: Padding(
+    //                 padding: EdgeInsets.all(mq.width * 0.05),
+    //                 child: Column(
+    //                   mainAxisAlignment: MainAxisAlignment.center,
+    //                   children: [
+    //                     Container(
+    //                         width: mq.width * 0.50,
+    //                         height: mq.width * 0.50,
+    //                         decoration: const BoxDecoration(
+    //                             image: DecorationImage(
+    //                                 image: AssetImage(
+    //                                     "assets/img/img-login.png")))),
+    //                     CustomField(
+    //                         controller: usernameController,
+    //                         hintText: 'Nombre de usuario',
+    //                         icon: Icons.account_circle_rounded),
+    //                     SizedBox(
+    //                       height: mq.height * 0.03,
+    //                     ),
+    //                     CustomField(
+    //                       controller: passwordController,
+    //                       hintText: 'Contraseña',
+    //                       icon: Icons.key,
+    //                       obscureText: true,
+    //                       isObscureText: true,
+    //                     ),
+    //                     SizedBox(
+    //                       height: mq.height * 0.03,
+    //                     ),
+    //                     CustomButton(
+    //                         mq: mq,
+    //                         color: blueColor,
+    //                         colorText: whiteColor,
+    //                         function: () async {
+    //                           FocusScope.of(context).unfocus();
+
+    //                           final authService = Provider.of<AuthService>(
+    //                               context,
+    //                               listen: false);
+
+    //                           if (usernameController.text == '' ||
+    //                               passwordController.text == '') {
+    //                             CustomShowDialog.alert(
+    //                                 context: context,
+    //                                 title: 'Error',
+    //                                 message:
+    //                                     'Debes de diligenciar los campos obligatorios.');
+    //                             return false;
+    //                           }
+
+    //                           final Map? resp = await authService.login(
+    //                               usuario: usernameController.text,
+    //                               password: passwordController.text);
+
+    //                           if (resp!['type'] == 'error') {
+    //                             CustomShowDialog.alert(
+    //                                 context: context,
+    //                                 title: 'Error',
+    //                                 message: resp['message']);
+    //                             return false;
+    //                           } else {
+    //                             Navigator.pushReplacementNamed(context, 'home');
+    //                           }
+    //                         },
+    //                         text: 'Ingresar'),
+    //                     SizedBox(
+    //                       height: mq.height * 0.03,
+    //                     ),
+    //                   ],
+    //                 ),
+    //               ),
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
     return Scaffold(
       body: GestureDetector(
         onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
@@ -121,23 +231,35 @@ class _LoginPageState extends State<LoginPage> {
                               }
                             },
                             text: 'Ingresar'),
-                        // (uiProvider.connectionStatusProvider ==
-                        //         ConnectivityResult.wifi)
-                        //     ? Icon(
-                        //         Icons.wifi,
-                        //         color: uiProvider.isDeviceConnectedProvider
-                        //             ? greenColor
-                        //             : redColor,
-                        //       )
-                        //     : (uiProvider.connectionStatusProvider ==
-                        //             ConnectivityResult.mobile)
-                        //         ? Icon(
-                        //             Icons.cell_tower,
-                        //             color: uiProvider.isDeviceConnectedProvider
-                        //                 ? greenColor
-                        //                 : redColor,
-                        //           )
-                        //         : const Icon(Icons.public_off)
+                        SizedBox(
+                          height: mq.height * 0.03,
+                        ),
+                        GestureDetector(
+                          onTap: () async {
+                            const url =
+                                'https://gestionatucuenta.tigoune.com/sigma/app/index?breakGlass=true#/forgot-password'; // Modifica con tu URL
+                            if (await canLaunchUrl(Uri.parse(url))) {
+                              await launchUrl(Uri.parse(url));
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('No se pudo abrir el enlace.'),
+                                ),
+                              );
+                            }
+                          },
+                          child: Column(
+                            children: [
+                              Text(
+                                'Olvido su contraseña?',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
